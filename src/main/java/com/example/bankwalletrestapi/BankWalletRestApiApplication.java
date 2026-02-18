adad263c594502341ad6095efddf4af4133fba59
Ivan <ivan.georgiev03@abv.bg>
2026-02-18 15:30:24 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/bankwalletrestapi/BankWalletRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankwalletrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWalletRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWalletRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankwalletrestapi/BankWalletRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankwalletrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWalletRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
