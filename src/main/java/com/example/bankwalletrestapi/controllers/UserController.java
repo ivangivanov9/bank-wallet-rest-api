219441514b3badeb1d1051cc3021c71fcfa87f8a
Ivan <ivan.georgiev03@abv.bg>
2026-02-19 17:54:10 +0200

    Add getUserById and deleteUser endpoints

--- src/main/java/com/example/bankwalletrestapi/controllers/UserController.java
+++ src/main/java/com/example/bankwalletrestapi/controllers/UserController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,5 +23,19 @@ public class UserController {
         log.info("REST request to create user: {}", userCreateDto.getUsername());
         UserResponseDto createdUser = userService.createUser(userCreateDto);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        log.info("REST request to get user by id: {}", id);
+        UserResponseDto user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        log.info("REST request to delete user by id: {}", id);
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
