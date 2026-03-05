ebe952870dd76b2155655be1e04c89db487e5b76
Ivan <ivan.georgiev03@abv.bg>
2026-03-05 14:11:55 +0200

    fix: add password field to UserCreateDto and encode in createUser method

--- src/main/java/com/example/bankwalletrestapi/models/dtos/userDtos/UserCreateDto.java
+++ src/main/java/com/example/bankwalletrestapi/models/dtos/userDtos/UserCreateDto.java
@@ -15,4 +15,8 @@ public class UserCreateDto {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
 }
